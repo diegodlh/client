da672716da2cf368eb9bdbfd4275db69a2dea84a
Diego de la Hera <delahera@gmail.com>
2020-07-27 13:37:48 -0300

    Allow setting usePdfWhiteOverlay setting from parent document

--- src/annotator/config/index.js
+++ src/annotator/config/index.js
@@ -45,5 +45,6 @@ export default function configFrom(window_) {
     externalContainerSelector: settings.hostPageSetting(
       'externalContainerSelector'
     ),
+    usePdfWhiteOverlay: settings.hostPageSetting('usePdfWhiteOverlay'),
   };
 }
